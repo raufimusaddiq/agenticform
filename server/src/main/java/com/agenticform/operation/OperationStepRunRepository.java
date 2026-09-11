f46c42e8d5f8595e92d4625d917e40c1c061046e
Raufi Musaddiq <raufimusaddiq@gmail.com>
2026-09-11 23:50:53 +0700

    feat: support resumable operation step lookup

--- server/src/main/java/com/agenticform/operation/OperationStepRunRepository.java
+++ server/src/main/java/com/agenticform/operation/OperationStepRunRepository.java
@@ -3,8 +3,10 @@ package com.agenticform.operation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OperationStepRunRepository extends JpaRepository<OperationStepRunEntity, UUID> {
     List<OperationStepRunEntity> findAllByOperationRunIdOrderByPosition(UUID operationRunId);
+    Optional<OperationStepRunEntity> findByOperationRunIdAndPosition(UUID operationRunId, int position);
 }
