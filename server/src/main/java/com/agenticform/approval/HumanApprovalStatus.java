4ead7f5bdec935d00ae378b8d4d014fddb434119
Raufi Musaddiq <raufimusaddiq@gmail.com>
2026-09-11 22:05:43 +0700

    feat: record preauthorized native approvals

--- server/src/main/java/com/agenticform/approval/HumanApprovalStatus.java
+++ server/src/main/java/com/agenticform/approval/HumanApprovalStatus.java
@@ -3,6 +3,7 @@ package com.agenticform.approval;
 public enum HumanApprovalStatus {
     PENDING,
     AUTO_APPROVED,
+    PREAUTHORIZED,
     POLICY_DENIED,
     APPROVED,
     APPROVED_FOR_SESSION,
