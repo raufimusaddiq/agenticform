52ca15185f2a1678bedc70307acc75cac5a7409d
Raufi Musaddiq <raufimusaddiq@gmail.com>
2026-09-12 00:36:11 +0700

    feat: query git-backed project sources

--- server/src/main/java/com/agenticform/project/ProjectRepository.java
+++ server/src/main/java/com/agenticform/project/ProjectRepository.java
@@ -7,4 +7,5 @@ import java.util.UUID;
 public interface ProjectRepository extends JpaRepository<ProjectEntity, UUID> {
     boolean existsBySlug(String slug);
     boolean existsByRootDirectory(String rootDirectory);
+    boolean existsByRepositoryUrl(String repositoryUrl);
 }
