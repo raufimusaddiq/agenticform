b752e91434b27be37780740508203c49e4c08015
Raufi Musaddiq <raufimusaddiq@gmail.com>
2026-09-12 08:29:46 +0700

    fix: import servlet response for auth entrypoint

--- server/src/main/java/com/agenticform/config/SecurityConfig.java
+++ server/src/main/java/com/agenticform/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.agenticform.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
