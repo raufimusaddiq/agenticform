8de7fd8eb158dae129163364cc53e59a4f98e304
Raufi Musaddiq <raufimusaddiq@gmail.com>
2026-09-12 11:11:47 +0700

    fix: query all heartbeat-active node statuses

--- server/src/main/java/com/agenticform/node/ExecutionNodeRepository.java
+++ server/src/main/java/com/agenticform/node/ExecutionNodeRepository.java
@@ -2,6 +2,7 @@ package com.agenticform.node;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -10,4 +11,5 @@ public interface ExecutionNodeRepository extends JpaRepository<ExecutionNodeEnti
     Optional<ExecutionNodeEntity> findByName(String name);
     Optional<ExecutionNodeEntity> findByFingerprint(String fingerprint);
     List<ExecutionNodeEntity> findAllByStatusOrderByName(ExecutionNodeStatus status);
+    List<ExecutionNodeEntity> findAllByStatusInOrderByName(Collection<ExecutionNodeStatus> statuses);
 }
