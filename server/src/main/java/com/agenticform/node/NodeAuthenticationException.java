3d9f2ed6eeea3ba2ed99326665a59a1745dbd002
Raufi Musaddiq <raufimusaddiq@gmail.com>
2026-09-12 08:40:18 +0700

    feat: model node authentication failures explicitly

---
+++ server/src/main/java/com/agenticform/node/NodeAuthenticationException.java
@@ -0,0 +1,7 @@
+package com.agenticform.node;
+
+public class NodeAuthenticationException extends RuntimeException {
+    public NodeAuthenticationException(String message) {
+        super(message);
+    }
+}
