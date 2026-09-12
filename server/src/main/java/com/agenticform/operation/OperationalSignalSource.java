2729c5db862814025b243b4c54ff97b566f6fd95
Raufi Musaddiq <raufimusaddiq@gmail.com>
2026-09-12 12:02:19 +0700

    feat: add operational signal source

---
+++ server/src/main/java/com/agenticform/operation/OperationalSignalSource.java
@@ -0,0 +1,12 @@
+package com.agenticform.operation;
+
+public enum OperationalSignalSource {
+    OPERATION,
+    GITHUB_WORKFLOW,
+    SERVICE_HEALTH,
+    NODE_HEALTH,
+    AGENT_RUNTIME,
+    DEPLOYMENT,
+    BACKUP,
+    DISK
+}
